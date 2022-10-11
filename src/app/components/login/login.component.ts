268a8ebdbe8793d7bcf8ce610be39760308c59e1
Bruna <c1324737@interno.bb.com.br>
2022-10-11 16:46:37 -0300

    feat: realiza validação do login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'login-root',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class Login {
+export class LoginComponent implements OnInit {
 
+  form: FormGroup = new FormGroup({});
+  alert!: { text: string, show: boolean, class: string };
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      senha: new FormControl('', [Validators.required])
+    })
+  }
+
+  login(event: any): void {
+    event.preventDefault();
+
+    if (this.form.valid) {
+      this.userService
+        .findByEmail(this.form.value.email)
+        .subscribe(
+          (data: any) => {
+            const user = data[0];
+
+            if (user) {
+              if (this.form.value.senha === user.senha) {
+                console.log("usuário autenticado");
+                return;
+              }
+
+              return this.onAlert('Senha inválida', 'alert-danger', 2000);
+            }
+
+            return this.onAlert('Usuário não encontrado', 'alert-danger', 2000);
+
+          },
+          () => {
+            return this.onAlert('Usuário não encontrado', 'alert-danger', 5000);
+          }
+        )
+      return;
+    }
+
+
+    return this.onAlert('Informe email e senha', 'alert-danger', 2000);
+  }
+
+  onAlert(text: string, className: string, timeout: number): void {
+    this.alert = { text, show: true, class: className }
+
+    setTimeout(() => {
+      this.alert = {
+        text: '',
+        show: false,
+        class: ''
+      }
+    }, timeout);
+  }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -9,7 +9,11 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  get() : Observable<any> {
-    return this.http.get('leitor');
+  get(): Observable<any> {
+    return this.http.get('http://localhost:3000/leitor');
+  }
+
+  findByEmail(email: string): Observable<any> {
+    return this.http.get('http://localhost:3000/leitor?email='+ email);
   }
 }
