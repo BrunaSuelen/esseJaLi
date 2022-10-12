6d9068893999b430cf3951918cf143f249a1143d
Bruna <c1324737@interno.bb.com.br>
2022-10-11 22:52:03 -0300

    feat: Cria página de ranking

--- src/app/components/ranking/ranking.component.ts
+++ src/app/components/ranking/ranking.component.ts
@@ -8,20 +8,25 @@ import { UserService } from 'src/app/service/user.service';
 })
 export class RankingComponent implements OnInit {
 
+  ranking!: any[];
+
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getLeitores()
   }
 
   getLeitores(): any {
-    let postos = [];
     this.userService
       .get()
       .subscribe(
         (data: any) => {
-          data?.forEach((leitor: any) => {
-
+          console.log(data)
+          const ordeByPontos = data.sort(function (x: any, y: any) {
+            return x.pontos - y.pontos
           });
+
+          this.ranking = ordeByPontos.reverse().splice(0, 10);
         }
       )
   }
